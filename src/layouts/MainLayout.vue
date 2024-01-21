1175a3ea3ad8d94ffb45f3808c7f5d60d76d791d
daquiro <daquiro@pm.me>
2024-01-21 06:21:16 -0300

    init rama noticias

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,17 +3,17 @@
         <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
 
         <v-app-bar color="primary" prominent>
-            <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer" disabled></v-app-bar-nav-icon>
 
-            <v-toolbar-title>My files</v-toolbar-title>
+            <v-toolbar-title>Noticias</v-toolbar-title>
 
             <v-spacer></v-spacer>
 
-            <v-btn variant="text" icon="mdi-magnify"></v-btn>
+            <v-btn variant="text" icon="mdi-magnify" disabled></v-btn>
 
-            <v-btn variant="text" icon="mdi-filter"></v-btn>
+            <v-btn variant="text" icon="mdi-filter" disabled></v-btn>
 
-            <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+            <v-btn variant="text" icon="mdi-dots-vertical" disabled></v-btn>
         </v-app-bar>
 
         <v-navigation-drawer v-model="drawer" location="left" temporary>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,10 +4,13 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import {aliases,mdi} from 'vuetify/iconsets/mdi'
 import { createVuetify } from 'vuetify'
+import { md1 } from 'vuetify/blueprints'
+
 
 export default createVuetify({
-    components,
-    directives,
-    icons:{defaultSet: 'mdi',aliases, sets:{mdi,}}
+  blueprint: md1,
+  components,
+  directives,
+  icons: { defaultSet: 'mdi', aliases, sets: { mdi } }
 })
 
