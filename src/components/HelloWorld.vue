badcfc7788bc94fbf29f895c5c385c27254639ea
u92156 <daquiroga@proveedor.teco.com.ar>
2024-01-20 21:10:12 -0300

    inicio

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <h1>hola</h1>
+</template>
+
+<style scoped>
+
+#container {
+  column-gap: 10;
+  /* display: flex;
+  flex-direction: column;
+  align-content:end; */
+
+}
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/checkbox/checkbox.js';
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
